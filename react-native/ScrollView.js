bde443c8090d5309b31fc9092e93800a2cf55a0d
chanseop <99310356+chanseop@users.noreply.github.com>
2023-05-10 18:31:34 +0900

    Create ScrollView.js

---
+++ react-native/ScrollView.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import {Text,StyleSheet ,View, Button, ScrollView,Alert} from "react-native";
+
+
+export default class App extends Component{
+  onPressButton(){
+    alert("클릭했네요.")
+  }
+
+  render(){
+    return(
+      <ScrollView>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+        <Text>문장입니다.</Text>
+        <Button title={'Button'} onPress={this.onPressButton}/>
+      </ScrollView>
+    );
+  }
+}
